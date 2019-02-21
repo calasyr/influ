f5da9c7aa53938dea79e842c8c65d6a955a5a065
Alan Brown <calasyr@gmail.com>
2019-02-20 19:47:25 -0800

    Got it working (I think).

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,6 +4,7 @@ class Comment < ApplicationRecord
   def self.read_it_in
     file = File.read("/Users/alanbrown/Work/apps/influ/reddit.json")
     data = JSON.parse(file)
+
     Comment.parse data
   end
 
@@ -24,27 +25,45 @@ class Comment < ApplicationRecord
   end
 
   def self.parse(obj)
-
     rea = lambda do |comment, parent_id=nil|
-      # username = 
-      user = User.find_by_name(username)
-      if user.nil?
-        user = User.create(name: username)
-      end
-      input = {
-        parent_comment_id: parent_id,
-        user_id: user.id
-      }
-      new_comment = Comment.create(input)
-      return if comment['data']['children'].nil?
-      comment['data']['children'].each do |child|
-        rea.call(child, new_comment.id)
+      begin
+        username = comment['data']['author']
+
+        user = User.find_by_name(username)
+        if user.nil?
+          user = User.create(name: username)
+        end
+        input = {
+          parent_comment_id: parent_id,
+          user_id: user.id
+        }
+        new_comment = Comment.create(input)
+
+        # puts comment['data']['replies']['data']['children']
+
+        # binding.pry
+        puts "new_comment.id is #{new_comment.id}"
+
+        replies = comment['data']['replies']
+        if replies && replies['data']['children'].any?
+          replies['data']['children'].each do |child|
+            rea.call(child, new_comment.id)
+          end
+        end
+      rescue => e
+        puts "e is #{e}"
       end
     end
 
-    obj.each do |h|
+    obj[0]['data']['children'].each do |h|
       rea.call(h)
     end
+
+
+    obj[1]['data']['children'].each do |h|
+      rea.call(h)
+    end
+
   end
 
 end
