3bd20b9cc24d7d58e4085a47e6596c29dfea7d93
Alan Brown <calasyr@gmail.com>
2019-02-20 20:14:21 -0800

    Removed unneeded code.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -8,43 +8,26 @@ class Comment < ApplicationRecord
     Comment.parse data
   end
 
-  def self.get_it_all
-    uri = URI.parse("https://www.reddit.com/r/NetflixBestOf/comments/7p8s2b/us_the_prestige_2006_desperate_to_reveal_each.json")
-
-    request = Net::HTTP::Get.new(uri.request_uri)
-
-    Comment.parse(api_request(request, uri))
-  end
-
-  def self.api_request(request, uri)
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    response = http.request(request)
-
-    JSON.parse(response.body)
-  end
-
   def self.parse(obj)
     rea = lambda do |comment, parent_id=nil|
       begin
         username = comment['data']['author']
 
         user = User.find_by_name(username)
+
         if user.nil?
           user = User.create(name: username)
         end
+
         input = {
           parent_comment_id: parent_id,
           user_id: user.id
         }
+
         new_comment = Comment.create(input)
 
-        # puts comment['data']['replies']['data']['children']
-
-        # binding.pry
-        puts "new_comment.id is #{new_comment.id}"
-
         replies = comment['data']['replies']
+
         if replies && replies['data']['children'].any?
           replies['data']['children'].each do |child|
             rea.call(child, new_comment.id)
